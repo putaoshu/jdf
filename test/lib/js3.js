4a9e203c75eac4973e99fd598b9ab9bb00dd2377
putaoshu <putaoshu@126.com>
2014-03-09 18:47:34 +0800

    support css link combo and widget js,css combine;optimize function name and comments

---
+++ test/lib/js3.js
@@ -0,0 +1,1 @@
+define("/unit/test/test.js",function(require,a,b){var c=require("unit/lib/lib1.js");var d=require("http://cdn/lib2.js");var e=require("http://cdn/lib3.css");function f(){return"init"}return f});
