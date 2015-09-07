34ce609381d8c0e6e1c7d2468c2dd4c70626c1bf
chenxiaochun <sjzchenxiaochun@126.com>
2015-09-07 15:47:27 +0800

    [add]test files

---
+++ test/src/js8.js
@@ -0,0 +1,14 @@
+var fs = require("fs");
+var fs1 = require("fs");
+
+function a(){
+	 seajs.use("jdf1",function(jdf2){
+		 
+	 })
+}
+
+function b(){
+	 seajs.use("a/jdf2.js",function(jdf2){
+		 
+	 })
+}
